3584a71dc1b7668cb66a58d7d31698d1329d2f0a
Gabriel Ribeiro <g.ribeiro@uget.com.br>
2025-11-13 15:41:43 -0300

    fix: build

--- dist/main/server.js
+++ dist/main/server.js
@@ -10,7 +10,7 @@ const app = (0, express_1.default)();
 const PORT = Number(process.env.PORT) || 3000;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.use('/api', routes_1.default);
+app.use(routes_1.default);
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
--- src/main/server.ts
+++ src/main/server.ts
@@ -7,7 +7,7 @@ const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', routes);
+app.use(routes);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
